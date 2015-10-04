e7ee80278eaab014dd350838bfc97d04cef0d6b8
kpok <karol.pokrzywinski@gmail.com>
2015-10-04 11:58:30 +0200

    addInzynierkaProjekt

---
+++ InzynierkaProjekt/InzynierkaProjekt/Assets/Scripts/MenuScripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour {
+
+	public void sceneChange (string scaneName) 
+    {
+        Application.LoadLevel(scaneName);
+	}
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
