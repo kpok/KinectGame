fa06425e7be18751e9210565ccf85764a0c243f0
djmichol <mic.podbielski@wp.pl>
2015-10-07 21:03:37 +0200

    commit
    Remove new unity project 1, add menu and map sontroller to dawid
    project.

---
+++ proj/New Unity Project/Assets/Scripts/Game scripts/mapScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapScript : MonoBehaviour {
+
+    public GameObject map;
+
+    private float startPnt = 50f;
+    private float endPnt = 1f;
+    private float time = 0;
+    private float speed = 0.5f;
+    private float posY = 0.3f;
+    // Update is called once per frame
+    void Update()
+    {       
+            Vector3 startPoint = new Vector3(0f, posY, startPnt);
+            Vector3 endPoint = new Vector3(0f,posY,endPnt);
+            transform.position = Vector3.Lerp(startPoint, endPoint, time);
+            startPnt-=speed;
+            endPnt-=speed;
+            time += Time.deltaTime * speed;
+        
+    }
+}
---
+++ proj/New Unity Project/Assets/Scripts/Menu script/menuMenager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class menuMenager : MonoBehaviour
+{
+
+    public void sceneChange(string scaneName)
+    {
+        Application.LoadLevel(scaneName);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
