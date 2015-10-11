622d2ef903a17045cb821382a37a2ec66f397720
djmichol <mic.podbielski@wp.pl>
2015-10-11 19:19:49 +0200

    MapUpdate
    Add mapController and new scene with map.

--- KinectRunner/Assets/Scripts/Game scripts/SpawnScript.cs
+++ KinectRunner/Assets/Scripts/Game scripts/SpawnScript.cs
@@ -5,6 +5,7 @@ public class SpawnScript : MonoBehaviour {
 
 	public GameObject obstacle;
 	public GameObject powerup;
+    public GameObject pipe;
 	
 	float timeElapsed = 0;
 	float spawnCycle = 0.5f;
@@ -12,22 +13,30 @@ public class SpawnScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        int number = Random.Range(1, 4);
+
 		timeElapsed += Time.deltaTime;
 		if(timeElapsed > spawnCycle)
 		{
 			GameObject temp;
-			if(spawnPowerup)
+			if(number==1)
 			{
 				temp = (GameObject)Instantiate(powerup);
 				Vector3 pos = temp.transform.position;
 				temp.transform.position = new Vector3(Random.Range(-3, 4), pos.y, pos.z);
 			}
-			else
+			else if(number==2)
 			{
 				temp = (GameObject)Instantiate(obstacle);
 				Vector3 pos = temp.transform.position;
 				temp.transform.position = new Vector3(Random.Range(-3, 4), pos.y, pos.z);
 			}
+            else if (number == 3)
+            {
+                temp = (GameObject)Instantiate(pipe);
+                Vector3 pos = temp.transform.position;
+                temp.transform.position = new Vector3(Random.Range(-3, 4), pos.y, pos.z);
+            }
 			
 			timeElapsed -= spawnCycle;
 			spawnPowerup = !spawnPowerup;
---
+++ KinectRunner/Assets/Scripts/Game scripts/pipeScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pipeScript : MonoBehaviour {
+
+    public float objectSpeed = -0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, objectSpeed, 0);
+    }
+}
